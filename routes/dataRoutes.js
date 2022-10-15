2858a4407667375b1da28b48308f3b3828bf25f5
Esra Al <esra.wameed@yahoo.com>
2022-10-15 00:03:24 -0400

    created a get and post route for notesto be able to read notes as well as add notes

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -4,5 +4,13 @@ const dataSave = require("../db/dataSave");
 
 //creating GET requests
 newroute.get("/notes", function(req, res){
-    
-})
+    dataSave.fetchNote().then(newNotes => res.json(newNotes))
+    .catch(error=> res.status(404).json(error));
+});
+
+//handling POST request
+newroute.post("/notes", (req,res)=>{
+    dataSave.noteAdd(req.body).then((newNote)=> res.json(newNote)).catch(error => res.status(404).json(error));
+});
+
+module.exports = newroute;
